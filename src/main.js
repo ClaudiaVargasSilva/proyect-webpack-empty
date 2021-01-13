cb8e50ac54329c93ff8d43dfe6a124c1c631e933
ClaudiaVargasSilva <clau.vargas.silva@gmail.com>
2021-01-13 17:17:31 +0000

    standar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Main = ({ adds, resta }) => <h1 className="text-danger">Hola <i className="fas fa-globe fa-5x"></i> Mundo {adds(5, 5)} {resta(23, 6)}</h1>;
+const Main = ({ adds, resta }) => <h1 className="text-danger">Hello <i className="fas fa-globe fa-5x"></i> Hello {adds(5, 5)} {resta(23, 6)}</h1>;
 
 export default Main;
